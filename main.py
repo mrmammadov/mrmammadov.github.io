15993d097044e4ee0c08726049a31fa5468fb982
Qulu Mammadli <mammadov.qulu@gmail.com>
2022-03-08 01:16:12 +0100

    Test Article in order to fix the Github Pages Publishing source issue (#2)

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from jinja2 import Environment, FileSystemLoader
 
 env = Environment(loader=FileSystemLoader("./"))
 
-template = env.get_template("index.html")
+template = env.get_template("template.html")
 
 with open("article.md", "r") as markdown_file:
     article = markdown(markdown_file.read())
 
-with open("output.html", "w") as output_file:
+with open("index.html", "w") as output_file:
     content = template.render(article=article)
     output_file.write(content)
